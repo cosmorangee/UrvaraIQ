0ab58ded6d07b1afeaa0222088d6970ee4785d19
cosmorangee <aditi.s.yadav25@srltce.in>
2026-04-11 04:10:28 +0530

    big update: added features and fixes

--- static/script.js
+++ static/script.js
@@ -124,3 +124,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.classList.toggle("show-menu");
+}
